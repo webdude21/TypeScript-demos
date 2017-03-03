4e15aa8b7669c7e027c4123c9056ee9acc00d5f3
webdude <dimo.petrof@gmail.com>
2017-03-03 19:01:14 +0200

    added more methods to the generic Stack

--- src/generics/demo.ts
+++ src/generics/demo.ts
@@ -7,10 +7,19 @@ export interface Person {
 
 export function demonstrate(): void {
     let stack = new Stack<Person>();
-    stack.push({ name: "Goshko", age: 21 });
+    let goshko = { name: "Goshko", age: 21 };
+    stack.push(goshko);
+    stack.push({ name: "Ivan", age: 32 });
+    stack.push({ name: "Maria", age: 40 });
     let person = stack.peek();
 
     if (person) {
         console.log(`I'm ${person.name} and I'm ${person.age} years old!`);
+    }
+
+    console.log(`The stack {`)
+
+    for (let person of stack) {
+        console.log(person);
     }
 }
--- src/generics/stack.ts
+++ src/generics/stack.ts
@@ -3,4 +3,10 @@ export class Stack<T> {
     push = (item: T) => this.internalStore.push(item);
     pop = (): T | undefined => this.internalStore.shift();
     peek = (): T | undefined => this.internalStore[this.internalStore.length - 1];
+    contains = this.internalStore.includes;
+    [Symbol.iterator] = function* iterator() : IterableIterator<T> {
+        for (let i = this.internalStore.length - 1; i >= 0; i--) {
+            yield this.internalStore[i];
+        }
+    };
 }
