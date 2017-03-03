466ba70387af38b1ed9064f9b8464c1ee55073ef
webdude <dimo.petrof@gmail.com>
2017-03-03 19:09:04 +0200

    fixed missing  spasings

--- src/generics/stack.ts
+++ src/generics/stack.ts
@@ -4,7 +4,7 @@ export class Stack<T> {
     pop = (): T | undefined => this.internalStore.shift();
     peek = (): T | undefined => this.internalStore[this.internalStore.length - 1];
     contains = this.internalStore.includes;
-    [Symbol.iterator] = function* iterator() : IterableIterator<T> {
+    [Symbol.iterator] = function* iterator(): IterableIterator<T> {
         for (let i = this.internalStore.length - 1; i >= 0; i--) {
             yield this.internalStore[i];
         }
