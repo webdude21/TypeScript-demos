83aadc0da2d6d6fb7f51d3eed760bc1fad533c76
webdude <dimo.petrof@gmail.com>
2017-03-13 21:37:35 +0200

    enchased type-guards demo

--- src/types/type-guards.ts
+++ src/types/type-guards.ts
@@ -3,3 +3,11 @@ import { Mage, Warrior } from './union-intersection-types';
 export function isMage(char: Mage | Warrior): char is Mage {
     return !char || !(<Mage>char).mp;
 }
+
+export function printInfo(char: Mage | Warrior) {
+    if (isMage(char)) {
+        console.log(`The mage ${char.name} has ${char.mp} magic points!`);
+    } else {
+        console.log(`The warrior ${char.name} has ${char.dexterity} dexterity`);
+    }
+}
