945d230c073665ac1bc3fc0a87a15e90b5e3a523
dimo <dpetrov@objectsystems.com>
2017-03-21 10:10:30 +0200

    added more demos

---
+++ src/types/tuples.ts
@@ -0,0 +1,11 @@
+export function makePair(key: number, value: string): [number, string] {
+    return [key, value];
+}
+
+export function triplet(one: number, two: string, three: number): [number, string, number] {
+    return [one, two, three];
+}
+
+let res = triplet(5, 'Why', 3);
+
+res[0] = res[0] + res[2];
