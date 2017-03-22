493d24b2df331cef9519d05bf46bb078e76614e6
dimo <dpetrov@objectsystems.com>
2017-03-22 19:01:13 +0200

    added tuple usage

--- src/types/tuples.ts
+++ src/types/tuples.ts
@@ -9,3 +9,9 @@ export function triplet(one: number, two: string, three: number): [number, strin
 let res = triplet(5, 'Why', 3);
 
 res[0] = res[0] + res[2];
+
+export function useTuples(tuple: any[]) {
+    tuple.forEach(x => console.log(x));
+}
+
+useTuples(res);
